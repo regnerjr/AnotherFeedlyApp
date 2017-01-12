e2587936b134e17488b9cf292bc1ad5b5480d965
John Regner <regnerjr@gmail.com>
2017-01-11 17:57:11 -0800

    Initial Commit

---
+++ AnotherFeedlyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnotherFeedlyApp
+//
+//  Created by John Regner on 1/11/17.
+//  Copyright © 2017 iOS-Connect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
