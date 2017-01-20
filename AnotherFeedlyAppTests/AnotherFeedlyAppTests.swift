6ec63036aa4ccba0a2061601ba9f66e9b9528439
John Regner <regnerjr@gmail.com>
2017-01-19 20:53:25 -0800

    Remove unused test

--- AnotherFeedlyAppTests/AnotherFeedlyAppTests.swift
+++ AnotherFeedlyAppTests/AnotherFeedlyAppTests.swift
@@ -23,13 +23,6 @@ class AppDelegateTests: XCTestCase {
         XCTAssertNotNil(appDelegate.appCoordinator)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
 
 class AppCoordinatorTests: XCTestCase {
@@ -161,7 +154,5 @@ class SignInViewControllerTests: XCTestCase {
                 print("Error: \(error)")
             }
         }
-
     }
-
 }
