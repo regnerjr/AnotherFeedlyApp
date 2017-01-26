3ffad583eb522999fccf8230b9dcd143cd10c3b5
John Regner <regnerjr@gmail.com>
2017-01-25 20:29:17 -0800

    Extract Auth struct to own file

--- AnotherFeedlyApp/Spotify.swift
+++ AnotherFeedlyApp/Spotify.swift
@@ -1,25 +1,5 @@
 import Foundation
-import Keys
 
-struct Auth {
-
-    let redirectUri: String
-    let scope: String
-    let clientId: String
-    let clientSecret: String
-
-    init(clientId: String = AnotherFeedlyAppKeys().fEEDLY_CLIENT,
-          clientSecret: String = AnotherFeedlyAppKeys().fEEDLY_SECRET,
-          redirectUri: String = "https://localhost/",
-          scope: String = "https://cloud.feedly.com/subscriptions"
-        ) {
-
-        self.clientId = clientId
-        self.clientSecret = clientSecret
-        self.redirectUri = redirectUri
-        self.scope = scope
-    }
-}
 
 struct Spotify {
 
