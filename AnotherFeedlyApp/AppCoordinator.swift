56fa45a778bc3adb7c74072a627b9653a9815321
John Regner <regnerjr@gmail.com>
2017-02-21 21:33:13 -0800

    Move URLExtensions, Re-order sidebar, move more work to feedly class

--- AnotherFeedlyApp/AppCoordinator.swift
+++ AnotherFeedlyApp/AppCoordinator.swift
@@ -14,38 +14,11 @@ class AppCoordinator {
     func appLaunched(options: [UIApplicationLaunchOptionsKey : Any]? = nil) {
 
         let signInVC = StoryboardScene.Main.instantiateWebViewController()
-        let signInWebViewDelegate = InterestingWebViewDelegate(completion: signInComplete,
+        let signInWebViewDelegate = InterestingWebViewDelegate(completion: feedly.signInFinished,
                                                           urlOfInterest: auth.redirectUri)
         signInVC.urlRequest = feedly.signInRequest
         signInVC.webViewDelegate = signInWebViewDelegate
         window.rootViewController = signInVC
     }
 
-    func signInComplete(withRedirectRequest request: URLRequest) {
-        print("OMG Sign In Complete")
-        guard let code = request.extractAuthCodeFromRedirect() else {
-            print("Called us back but with no code?")
-            return
-        }
-        feedly.requestToken(withCode: code, completion: saveToken)
-    }
-
-    func saveToken(token: FeedlyToken) {
-        print("SavingToken")
-    }
-
-}
-
-extension URLRequest {
-    func extractAuthCodeFromRedirect() -> String? {
-        guard let url = url else { return nil }
-        let components = NSURLComponents(string: url.absoluteString)
-        guard let items = components?.queryItems else {
-            return nil
-        }
-        let code = items.filter { (item) -> Bool in
-            return item.name == "code"
-        }
-        return code.first?.value
-    }
 }
